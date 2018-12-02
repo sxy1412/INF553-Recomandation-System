3f9e90ab957fb016413b4ee156816032cad06652
yueqiz <45524583+yueqiz@users.noreply.github.com>
2018-12-01 17:34:03 -0800

    Add files via upload

---
+++ user_similarity/build_user_similarity.py
@@ -0,0 +1,48 @@
+import pandas as pd
+import math
+import sys
+
+def find_similarity(user):
+    #rtn={}
+    userlist=[]
+    simlist=[]
+    count=0
+    for other_user in users:
+        count+=1
+        print(count)
+        if user==other_user:
+            
+            continue
+        restaurants1=df.loc[df.user_id==user,"business_id"].values.tolist()
+        restaurants2=df.loc[df.user_id==other_user,"business_id"].values.tolist()
+        restaurants=set(restaurants1) & set(restaurants2)
+        mean1=df.loc[df.user_id==user,"rating"].mean()
+        mean2=df.loc[df.user_id==other_user,"rating"].mean()
+        up=0.0
+        down1=0.0
+        down2=0.0
+        for rest in restaurants:
+            up+=(rating[(user,rest)]-mean1)*(rating[(other_user,rest)]-mean2)
+            down1+=pow(rating[(user,rest)]-mean1,2)
+            down2+=pow(rating[(other_user,rest)]-mean1,2)
+        #rtn[other_user]=up/(math.sqrt(down1)*math.sqrt(down2))
+        userlist.append(other_user)
+        if down1*down2==0:
+            simlist.append(0)
+        else:
+            simlist.append(up/(math.sqrt(down1)*math.sqrt(down2)))
+    return pd.DataFrame({"user_id":userlist,"similarity":simlist})
+            
+
+df=pd.read_csv(sys.argv[0])
+users_f=open(sys.argv[1],'r')
+users=set()
+for line in users_f.readlines():
+    users.add(line[:-1])
+
+rating={}
+
+for i in df.index:
+    rating[(df.loc[i,"user_id"],df.loc[i,"business_id"])]=df.loc[i,"rating"]
+
+find_similarity("AuIK5tF2GjO7SftHawTLKw").to_csv("user_similarity.csv")
